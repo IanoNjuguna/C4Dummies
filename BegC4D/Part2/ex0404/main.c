30afcc992571d156c9ccd851a342331a107c588f
IanoNjuguna <ichege438@gmail.com>
2022-06-17 16:40:19 +0300

    ex0404

---
+++ BegC4D/Part2/ex0404/main.c
@@ -0,0 +1,13 @@
+//A program that prompts the standard output to display multiple lines of text
+
+#include <stdio.h>
+
+int main()
+{
+    puts("Hickory, dickory, dock,");
+    puts("The mouse ran up the clock.");
+    puts("The clock struck one,");
+    puts("The mouse ran down the dock,");
+    puts("Hickory, dickory, dock.");
+    return 0;
+}
