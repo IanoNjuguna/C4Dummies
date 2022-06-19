298ca1ca45c68b86868330f7556667197175776f
IanoNjuguna <ichege438@gmail.com>
2022-06-20 01:36:39 +0300

    Introduction

--- CProgdotCom/Intro-to-C/intro.c
+++ CProgdotCom/Intro-to-C/intro.c
@@ -1,10 +1,11 @@
-//The getchar() function
+//Introduction to C
 #include <stdio.h>
 
+//main function
 int main()
 {
     printf("Happy programmers write happy code.\n");
-    //getchar();
+    getchar();
     //getchar() reads in a single character and waits for the user to hit enter before reading the character.
     //It keeps that window from closing because the program is not done yet because it waits for you to hit enter.
     //Including getchar() gives you time to see the program run
