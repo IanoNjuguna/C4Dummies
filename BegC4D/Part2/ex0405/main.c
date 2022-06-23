acab755b92d7b5d27567802788b4fe1b26fa5914
IanoNjuguna <ichege438@gmail.com>
2022-06-23 14:07:12 +0300

    Adding ex0405

---
+++ BegC4D/Part2/ex0405/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    puts("The secret password is: ");
+    /*                               puts("Spatula.");  */
+    return 0;
+}
+
+// Type /* at the start of Line 6, then press the Tab key to indent the
+// statement to the same tab stop as on the preceding line. Press Tab at the end
+// of Line 6 before adding the final comment marker: */
+// . Save. Build. Run.
