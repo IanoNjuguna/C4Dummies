839e8c6cc3bd5313e9548e57f4c8e02d1ca9e869
IanoNjuguna <ichege438@gmail.com>
2022-06-23 15:21:44 +0300

    Commenting out getchar(); @ ex0411

--- BegC4D/Part2/ex0411/main.c
+++ BegC4D/Part2/ex0411/main.c
@@ -8,3 +8,8 @@ int main()
     return 0;
 }
 
+// I commented out getchar(); 
+// When I run this code on Code::Blocks, it runs in less than a second
+// but for some reason, the source code takes longer to run on VS Code(more than 10 minutes) 
+// presumably, it's because of the getchar() function. I don't know why so.
+// Hopefully, I'll find out why as I continue my journey. Any help will be appreciated.
