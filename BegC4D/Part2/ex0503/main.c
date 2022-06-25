2cc0fd748449e00cd19cb891144aeaa0568c0185
IanoNjuguna <ichege438@gmail.com>
2022-06-25 15:18:42 +0300

    ex0503

---
+++ BegC4D/Part2/ex0503/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%d.\n",127);
+    printf("%1.2f\n",3.1415926535);
+    printf("%d.\n",122013);
+    printf("%1.1f",0.00008);
+    return 0;
+}
