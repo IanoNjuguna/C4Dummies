cfe5fea5d8d5c923908b90642d5f2670c6df52f8
IanoNjuguna <ichege438@gmail.com>
2022-06-28 11:51:26 +0300

    ex0514

---
+++ BegC4D/Part2/ex0514/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define A 8
+#define B 2
+
+int main()
+{
+    puts("The values are 8 and 2:");
+    printf("Addition is %d\n", A+B);
+    printf("Subtraction is %d\n",A-B);
+    printf("Multiplication is %d\n",A*B);
+    printf("Division is %d\n",A/B);
+    return 0;
+}
