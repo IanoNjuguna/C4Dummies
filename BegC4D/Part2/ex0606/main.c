16b745c23f0bedc68c4d92cf2eea85c2e5e12f56
IanoNjuguna <ichege438@gmail.com>
2022-07-02 16:02:50 +0300

    ex0606

---
+++ BegC4D/Part2/ex0606/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int GLORKUS;
+
+    GLORKUS=22;
+
+    printf("The value of GLORKUS is %d\n",GLORKUS);
+    printf("The value of GLORKUS plus 16 is %d\n",GLORKUS+16);
+    printf("The value of GLORKUS times itself is %d\n",GLORKUS*GLORKUS);
+    return(0);
+}
