7bb8444bb8499612344df65a6e896ff1fe40b30b
IanoNjuguna <ichege438@gmail.com>
2022-07-17 22:29:41 +0300

    ex0705

---
+++ BegC4D/Part2/ex0705/main.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main()
+{
+    int ch;
+
+    printf("Press Enter Key: ");
+    getchar();
+    ch='H';
+    putchar(ch);
+    ch='i';
+    putchar('!');
+    return 0;
+}
