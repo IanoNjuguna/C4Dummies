e813fa92c42766e5ac3eb6e8cabee7539e7093b5
IanoNjuguna <ichege438@gmail.com>
2022-07-26 19:22:39 +0300

    ex0710

---
+++ BegC4D/Part2/ex0710/main.c
@@ -0,0 +1,16 @@
+/* Iano Njuguna
+ * https://twitter.com/MsanidiProgramu
+ * https://www.twitch.tv/ianonjuguna
+ * https://www.instagram.com/ianonjuguna_/
+ * https://www.tiktok.com/@njugunatok
+ */
+#include <stdio.h>
+
+int main ()
+{
+    char prompt [] = "Press Enter: ";
+
+    printf("%s",prompt);
+    getchar();
+    return 0;
+}
