093f88195c51cd6fa69e7602432727a8c022ccad
IanoNjuguna <ichege438@gmail.com>
2022-07-26 19:13:50 +0300

    ex0709

---
+++ BegC4D/Part2/ex0709/main.c
@@ -0,0 +1,25 @@
+/* Iano Njuguna
+ * https://twitter.com/MsanidiProgramu
+ * https://www.twitch.tv/ianonjuguna
+ * https://www.instagram.com/ianonjuguna_/
+ * https://www.tiktok.com/@njugunatok
+ */
+
+#include <stdio.h>
+
+int main ()
+{
+    
+    char a,b,c,d;
+    
+    a = 'W';
+    b = 'o';
+    c = 'w';
+    d = '\n';
+    
+    putchar (a);
+    putchar (b);
+    putchar (c);
+    putchar (d);
+    return 0;
+}
