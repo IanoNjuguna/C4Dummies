9b662af918d6042147739a57ab76d46618f74e34
IanoNjuguna <ichege438@gmail.com>
2022-07-26 19:35:39 +0300

    ex0711

---
+++ BegC4D/Part2/ex0711/main.c
@@ -0,0 +1,19 @@
+/* Iano Njuguna
+ * https://twitter.com/MsanidiProgramu
+ * https://www.twitch.tv/ianonjuguna
+ * https://www.instagram.com/ianonjuguna_/
+ * https://www.tiktok.com/@njugunatok
+ */
+
+#include <stdio.h>
+
+int main ()
+{
+    char prompt_0 [] = "Program to destroy the world";
+    char prompt_1 [] = "Press Enter:";
+
+    printf("%s\n",prompt_0);
+    printf("%s\n",prompt_1);
+    getchar();
+    return 0;
+}
