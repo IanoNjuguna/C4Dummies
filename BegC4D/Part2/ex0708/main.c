428377b7e43601a81d4a81a52c8c8053f6aeb4ae
IanoNjuguna <ichege438@gmail.com>
2022-07-26 11:14:15 +0300

    ex0708

---
+++ BegC4D/Part2/ex0708/main.c
@@ -0,0 +1,18 @@
+/* Iano Njuguna
+ * https://github.com/IanoNjuguna
+ */
+
+#include <stdio.h>
+
+int main ()
+{
+    char a,b,c,d;
+
+    a = 'W';
+    b = 'o';
+    c = 'w';
+    d = '\n';
+
+    printf("%c%c%c%c",a,b,c,d);
+    return 0;
+}
