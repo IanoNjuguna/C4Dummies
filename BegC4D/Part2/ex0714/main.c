a33e0bf69f4a5355eb36af0f7bb6292558874123
IanoNjuguna <ichege438@gmail.com>
2022-07-27 08:57:55 +0300

    ex0714

---
+++ BegC4D/Part2/ex0714/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+    int fav;
+
+    printf("What is your favourite number: ");
+    scanf("%d",&fav);
+    printf("%d is my favourite number, too!\n",fav);
+    return 0;
+}
