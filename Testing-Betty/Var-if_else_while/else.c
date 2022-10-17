148e21929b0c0a178ccf3c23e5ef948e120d6c7d
IanoNjuguna <ichege438@gmail.com>
2022-10-17 03:12:41 +0300

    else.c/else.out

---
+++ Testing-Betty/Var-if_else_while/else.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main(void)
+{
+	int a = 7;
+	int b = 3;
+
+	if (a == b)
+	{
+		printf("Ukweli kabisa\n");
+	}
+	else
+	{
+		printf("Wacha uongo wewe\n");
+	}
+	return (0);
+}
