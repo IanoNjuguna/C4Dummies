dd08bd1dc4516aeb04bd3fab6412896094a52f92
IanoNjuguna <ichege438@gmail.com>
2022-10-26 16:44:30 +0300

    Arithmetic (Multiplication)

--- BegC4D/Part1/ex0301Update1/main.c
+++ BegC4D/Part1/ex0301Update1/main.c
@@ -1,9 +1,14 @@
-//A basic C Program that calculates 4 * 5
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main ()
-
+/**
+ * main - Entry point.
+ * Description: A basic C Program that calculates 4 * 5.
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    printf("4 times 5 is %d\n", 4 * 5);
-    return(0);
+printf("4 times 5 is %d\n", 4 * 5);
+return(0);
 }
