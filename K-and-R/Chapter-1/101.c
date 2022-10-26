b86711f9c7844f134b7afa3b72929d9170e77815
IanoNjuguna <ichege438@gmail.com>
2022-10-26 17:18:44 +0300

    101 && 102

---
+++ K-and-R/Chapter-1/101.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point.
+ * Description: Run the "hello, world" program on your system
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+int main(void)
+{
+printf("Hello World.\n");
+return (0);
+}
---
+++ K-and-R/Chapter-1/102.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point.
+ * Description: What happens when printf's argument string contains '\c
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+int main(void)
+{
+printf("Hello World.\c");
+return (0);
+}
