f9618f731044178347d523092b545d9c708a169c
IanoNjuguna <ichege438@gmail.com>
2022-10-26 16:18:39 +0300

    Dummy Program

--- BegC4D/Part1/ex0301/main.c
+++ BegC4D/Part1/ex0301/main.c
@@ -1,3 +1,13 @@
-//A simple program that does nothing(Dummy program)
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-main() {}
+/**
+ * main - Entry point.
+ * Description: A simple program that does nothing(Dummy program)
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+return (0);
+}
