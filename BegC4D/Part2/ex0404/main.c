e5db786ab0697cfa4095e0c5b5ace22f29102a26
IanoNjuguna <ichege438@gmail.com>
2022-11-01 23:46:16 +0300

    ex0404 && Build

--- BegC4D/Part2/ex0404/main.c
+++ BegC4D/Part2/ex0404/main.c
@@ -1,13 +1,19 @@
-//A program that prompts the standard output to display multiple lines of text
-
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point.
+ * Description: Program that prompts the standard output
+ *		to display multiple lines of text.
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
 {
-    puts("Hickory, dickory, dock,");
-    puts("The mouse ran up the clock.");
-    puts("The clock struck one,");
-    puts("The mouse ran down the dock,");
-    puts("Hickory, dickory, dock.");
-    return 0;
+	puts("Hickory, dickory, dock,");
+	puts("The mouse ran up the clock.");
+	puts("The clock struck one,");
+	puts("The mouse ran down the dock,");
+	puts("Hickory, dickory, dock.");
+	return (0);
 }
