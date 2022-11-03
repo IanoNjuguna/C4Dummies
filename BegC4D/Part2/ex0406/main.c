eb83542e22003e21dd8fa7260e289e116413b2d7
IanoNjuguna <ichege438@gmail.com>
2022-11-03 11:50:33 +0300

    ex0406 && Build

--- BegC4D/Part2/ex0406/main.c
+++ BegC4D/Part2/ex0406/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point.
+ * Description: Program that uses puts()
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+int main(void)
 {
-    puts("The secret password is:");
-    puts("Spatula");
-    return 0;
+	puts("The secret password is:");
+	puts("Spatula");
+	return (0);
 }
