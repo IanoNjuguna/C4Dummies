c8368f5f3ce899b88e3942296ee3d3cd2e4eaa0e
IanoNjuguna <ichege438@gmail.com>
2022-11-03 12:08:31 +0300

    ex0407 && Build

--- BegC4D/Part2/ex0407/main.c
+++ BegC4D/Part2/ex0407/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+/**
+ * main - Entry point.
+ * Description: Program that disables a statement using comments.
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
 {
-    //              puts("The secret password is:");
-    puts("Spatula");
-    return 0;
+	/*              puts("The secret password is:");    */
+	puts("Spatula");
+	return (0);
 }
