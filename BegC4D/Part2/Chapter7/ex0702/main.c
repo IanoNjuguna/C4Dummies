51efd61c35b60f835b3c8e797a9687df924998e7
IanoNjuguna <ichege438@gmail.com>
2022-11-03 14:33:28 +0300

    ex0702 && Build

--- BegC4D/Part2/Chapter7/ex0702/main.c
+++ BegC4D/Part2/Chapter7/ex0702/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+/**
+ * main - Entry point.
+ * Description: Program that displays a
+ *		character’s ASCII code value.
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+
+int main(void)
 {
-    int c;
+	int c;
 
-    printf("I'm waiting for a character: ");
-    c = getchar();
-    printf("I waited for the '%d' character.\n",c);
-    return 0;
+	printf("I'm waiting for a character: ");
+	c = getchar();
+	printf("I waited for the '%d' character.\n", c);
+	return (0);
 }
