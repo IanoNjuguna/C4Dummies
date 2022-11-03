47a30ce1a398d7472b71a3d2520074329a54727b
IanoNjuguna <ichege438@gmail.com>
2022-11-03 12:37:28 +0300

    ex 0414/12 && Build(s)

--- BegC4D/Part2/ex0412/main.c
+++ BegC4D/Part2/ex0412/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point.
+ * Description: Program that uses displays
+ *              multiple lines of text using printf()
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+
+int main(void)
 {
-    printf("Hickory, dickory, dock,\n");
-    printf("The mouse ran up the clock.\n");
-    printf("The clock struck one,\n");
-    printf("The mouse ran down the dock,\n");
-    printf("Hickory, dickory, dock.\n");
-    return 0;
+	printf("Hickory, dickory, dock,\n");
+	printf("The mouse ran up the clock.\n");
+	printf("The clock struck one,\n");
+	printf("The mouse ran down the dock,\n");
+	printf("Hickory, dickory, dock.\n");
+	return (0);
 }
