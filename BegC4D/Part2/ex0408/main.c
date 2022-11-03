d7abb777b82e6531f310736ec65249bf6adee494
IanoNjuguna <ichege438@gmail.com>
2022-11-03 12:10:31 +0300

    ex0408

--- BegC4D/Part2/ex0408/main.c
+++ BegC4D/Part2/ex0408/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point.
+ * Description: Goofing around. Program will not compile {line 12}.
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+int main(void)
 {
     puts("This program goes BOOM!)
-    return 0;
+    return (0);
 }
-
-//                              GOOFING AROUND
