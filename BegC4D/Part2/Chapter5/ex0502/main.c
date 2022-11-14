beea103756de424c3175842ab0215da4dad58b33
IanoNjuguna <ichege438@gmail.com>
2022-11-14 14:39:32 +0300

    ex0502 && Build

--- BegC4D/Part2/Chapter5/ex0502/main.c
+++ BegC4D/Part2/Chapter5/ex0502/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main -Entry point.
+ * Description: Displaying integers and floats
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+
+int main(void)
 {
-    printf("%d.\n",127);
-    printf("%f.\n",3.1415926535);
-    printf("%d.\n",122013);
-    printf("%f.\n",0.00008);
-    return 0;
+	printf("%d.\n", 127);
+	printf("%f.\n", 3.1415926535);
+	printf("%d.\n", 122013);
+	printf("%f.\n", 0.00008);
+	return (0);
 }
