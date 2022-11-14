c60f8235325b2c593adada101db519cba5d0691b
IanoNjuguna <ichege438@gmail.com>
2022-11-14 20:00:46 +0300

    ex 0609, ex 0610 && Builds

--- BegC4D/Part2/Chapter6/ex0610/main.c
+++ BegC4D/Part2/Chapter6/ex0610/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main -Entry point.
+ * Description: working with various data types
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+int main(void)
 {
-    int shadrach=700;
-    int meshach=800;
-    int abednego=5600;
+	int shadrach = 700;
+	int meshach = 800;
+	int abednego = 5600;
 
-    printf("Shadrach is %d\nMeshach is %d\nAbednego is %d",shadrach,meshach,abednego);
-    return 0;
+	printf("Shadrach is %d\n", shadrach);
+	printf("Meshach is %d\n", meshach);
+	printf("Abednego is %d\n", abednego);
+	return (0);
 }
