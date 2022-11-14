937129461e97f4681aeccf0fe8dd7b48c0dfaab0
IanoNjuguna <ichege438@gmail.com>
2022-11-14 19:34:20 +0300

    ex0605 && Build

--- BegC4D/Part2/Chapter6/ex0605/main.c
+++ BegC4D/Part2/Chapter6/ex0605/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-    int blorf;
+	int blorf;
 
-    blorf=22;
+	blorf = 22;
 
-    printf("The value of blorf is %d\n",blorf);
-    printf("The value of blorf plus 16 is %d\n",blorf+16);
-    printf("The value of blorf times itself is %d\n",blorf*blorf);
-    return(0);
+	printf("The value of blorf is %d\n", blorf);
+	printf("The value of blorf plus 16 is %d\n", blorf + 16);
+	printf("The value of blorf times itself is %d\n", blorf * blorf);
+	return (0);
 }
