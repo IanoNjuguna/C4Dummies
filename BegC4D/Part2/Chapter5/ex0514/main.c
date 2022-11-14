727a446158e9c15dd537dd7c916b1a2e40c928b6
IanoNjuguna <ichege438@gmail.com>
2022-11-14 15:49:00 +0300

    ex0514 && Build

--- BegC4D/Part2/Chapter5/ex0514/main.c
+++ BegC4D/Part2/Chapter5/ex0514/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main -Entry point.
+ * Description: defining a constant
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
 #define A 8
 #define B 2
 
-int main()
+int main(void)
 {
-    puts("The values are 8 and 2:");
-    printf("Addition is %d\n", A+B);
-    printf("Subtraction is %d\n",A-B);
-    printf("Multiplication is %d\n",A*B);
-    printf("Division is %d\n",A/B);
-    return 0;
+	puts("The values are 8 and 2:");
+	printf("Addition is %d\n", A + B);
+	printf("Subtraction is %d\n", A - B);
+	printf("Multiplication is %d\n", A * B);
+	printf("Division is %d\n", A / B);
+	return (0);
 }
