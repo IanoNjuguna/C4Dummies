8acccb5111919bd042bb1eb52735d46b5ab4e6d7
IanoNjuguna <ichege438@gmail.com>
2022-11-14 15:03:49 +0300

    ex0509Fix && Build

--- BegC4D/Part2/Chapter5/ex0509Fix/main.c
+++ BegC4D/Part2/Chapter5/ex0509Fix/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main -Entry point.
+ * Description: Using the printf() functions
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+int main(void)
 {
-    printf("The total is %f\n",16.0 + 17);
-    return 0;
+	printf("The total is %f\n", 16.0 + 17);
+	return (0);
 }
