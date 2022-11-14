135b2b2b3f9c473899963ec23891e909d961ca9f
IanoNjuguna <ichege438@gmail.com>
2022-11-14 19:21:40 +0300

    ex0601 && Build

--- BegC4D/Part2/Chapter6/ex0601/main.c
+++ BegC4D/Part2/Chapter6/ex0601/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main -Entry point.
+ * Description: defining a variable
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+int main(void)
 {
-    int x;
+	int x;
 
-    x=5;
-    printf("The value of variable x is %d.\n",x);
-    return 0;
+	x = 5;
+	printf("The value of variable x is %d.\n", x);
+	return (0);
 }
