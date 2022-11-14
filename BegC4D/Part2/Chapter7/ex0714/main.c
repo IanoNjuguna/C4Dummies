b0cdaf84ccd52174b46b921dc25c3596477448dc
IanoNjuguna <ichege438@gmail.com>
2022-11-14 12:43:08 +0300

    ex0713, ex0714 && Builds

--- BegC4D/Part2/Chapter7/ex0714/main.c
+++ BegC4D/Part2/Chapter7/ex0714/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+/**
+ * main - Entry point.
+ * Description: Reading integers using the scanf() function
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+int main(void)
 {
-    int fav;
-    
-    printf("What is your favourite number: ");
-    scanf("%d",&fav);
-    printf("%d is my favourite number, too!\n",fav);
-    return 0;
+	int fav;
+
+	printf("What is your favourite number: ");
+	scanf("%d", &fav);
+	printf("%d is my favourite number, too!\n", fav);
+	return (0);
 }
