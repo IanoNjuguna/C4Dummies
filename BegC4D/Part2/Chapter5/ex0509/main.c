938b3fe987c7ffe2d0d5129c695982ed5e77c2a0
IanoNjuguna <ichege438@gmail.com>
2022-11-14 15:01:07 +0300

    ex0509

--- BegC4D/Part2/Chapter5/ex0509/main.c
+++ BegC4D/Part2/Chapter5/ex0509/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main -Entry point.
+ * Description: Using the printf() functions
+ * Return: Brings an error during compiling
+ * Author: IanoNjuguna
+ */
 
-int main()
+int main(void)
 {
-    printf("The total is %d\n",16.0+17);
-    return 0;
+	printf("The total is %d\n", 16.0 + 17);
+	return (0);
 }
