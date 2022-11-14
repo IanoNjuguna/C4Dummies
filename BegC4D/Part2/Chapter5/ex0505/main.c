a4fa893d35b023a6d89a84ed71984d2aa9c0756b
IanoNjuguna <ichege438@gmail.com>
2022-11-14 14:47:45 +0300

    ex0505 && Build

--- BegC4D/Part2/Chapter5/ex0505/main.c
+++ BegC4D/Part2/Chapter5/ex0505/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main -Entry point.
+ * Description: Using the puts() and printf() functions
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+
+int main(void)
 {
-    puts("The values are 456.98 and 213.4:");
-    printf("Addition is %f\n",456.98 + 213.4);
-    return 0;
+	puts("The values are 456.98 and 213.4:");
+	printf("Addition is %f\n", 456.98 + 213.4);
+	return (0);
 }
