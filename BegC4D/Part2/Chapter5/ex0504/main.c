6a3c89bcc422b29f79a36cd61e61e9552270f5d1
IanoNjuguna <ichege438@gmail.com>
2022-11-14 14:44:50 +0300

    ex0504 && Build

--- BegC4D/Part2/Chapter5/ex0504/main.c
+++ BegC4D/Part2/Chapter5/ex0504/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main -Entry point.
+ * Description: Arithmetic Operators
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+int main(void)
 {
-    puts("Values 8 and 2:");
-    printf("Addition is %d\n",8+2);
-    printf("Subtraction is %d\n",8-2);
-    printf("Multiplication is %d\n",8*2);
-    printf("Division is %d\n",8/2);
-    return 0;
+	puts("Values 8 and 2:");
+	printf("Addition is %d\n", 8 + 2);
+	printf("Subtraction is %d\n", 8 - 2);
+	printf("Multiplication is %d\n", 8 * 2);
+	printf("Division is %d\n", 8 / 2);
+	return (0);
 }
