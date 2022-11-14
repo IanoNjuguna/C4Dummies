dc338e01515013c022b475d78609361d2fa27eee
IanoNjuguna <ichege438@gmail.com>
2022-11-14 12:03:25 +0300

    ex0709 && Build

--- BegC4D/Part2/Chapter7/ex0709/main.c
+++ BegC4D/Part2/Chapter7/ex0709/main.c
@@ -1,25 +1,23 @@
-/* Iano Njuguna
- * https://twitter.com/MsanidiProgramu
- * https://www.twitch.tv/ianonjuguna
- * https://www.instagram.com/ianonjuguna_/
- * https://www.tiktok.com/@njugunatok
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Description: Working with character variables using the printf() function
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
  */
 
-#include <stdio.h>
-
-int main ()
+int main(void)
 {
-    
-    char a,b,c,d;
-    
-    a = 'W';
-    b = 'o';
-    c = 'w';
-    d = '\n';
-    
-    putchar (a);
-    putchar (b);
-    putchar (c);
-    putchar (d);
-    return 0;
+	char a, b, c, d;
+
+	a = 'W';
+	b = 'o';
+	c = 'w';
+	d = '\n';
+
+	putchar (a);
+	putchar (b);
+	putchar (c);
+	putchar (d);
+	return (0);
 }
