6780c3c913a425477a51cc8498185993077a0ea1
IanoNjuguna <ichege438@gmail.com>
2022-11-14 11:52:49 +0300

    ex0706 && Build

--- BegC4D/Part2/Chapter7/ex0706/main.c
+++ BegC4D/Part2/Chapter7/ex0706/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+/**
+ * main - Entry point.
+ * Description: Program that fetches characters using getchar()
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main ()
+int main(void)
 {
-    int ch;
+	int ch;
 
-    printf("Press Enter Key: ");
-    getchar();
-    ch='H';
-    putchar(ch);
-    ch='i';
-    putchar('!');
-    putchar('\n');
-    return 0;
+	printf("Press Enter Key: ");
+	getchar();
+	ch = 'H';
+	putchar(ch);
+	ch = 'i';
+	putchar('!');
+	putchar('\n');
+	return (0);
 }
