79582e88f5d35f7f24b0e7b27501653795124366
IanoNjuguna <ichege438@gmail.com>
2022-11-14 19:27:38 +0300

    ex0603 && Build

--- BegC4D/Part2/Chapter6/ex0603/main.c
+++ BegC4D/Part2/Chapter6/ex0603/main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main -Entry point.
+ * Description: defining various data types
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
 
-int main()
+int main(void)
 {
-    char c;
-    int i;
-    float f;
-    double d;
+	char c;
+	int i;
+	float f;
+	double d;
 
-    c='a';
-    i=1;
-    f=19.0;
-    d=20000.009;
+	c = 'a';
+	i = 1;
+	f = 19.0;
+	d = 20000.009;
 
-    printf("%c\n%d\n%f\n%f\n",c,i,f,d);
-    return(0);
+	printf("%c\n%d\n%f\n%f\n", c, i, f, d);
+	return (0);
 }
