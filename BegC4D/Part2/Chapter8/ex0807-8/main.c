ac0be251bf82da3db4a7a27292c34ae4304d433b
IanoNjuguna <inc3099@gmail.com>
2022-12-20 01:40:18 +0300

    ex0807 && ex 0808

---
+++ BegC4D/Part2/Chapter8/ex0807-8/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Comparison Operators
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 5;
+
+	if (a == -3)
+	{
+		printf("%d equals %d\n", a, -3);
+	}
+	return (0);
+}
+
