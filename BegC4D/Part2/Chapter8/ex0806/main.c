70db3b0242133d8f77801d47f89f8f6d7ab68997
IanoNjuguna <inc3099@gmail.com>
2022-12-20 01:20:03 +0300

    ex0806

---
+++ BegC4D/Part2/Chapter8/ex0806/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define SECRET 17
+
+/**
+ * main - Entry point
+ * Description: Checks whether two values are equal to each other
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int guess;
+
+	printf("Can you guess the secret number: \n");
+	scanf("%d", &guess);
+
+	if (guess == SECRET)
+	{
+		puts("You guessed it!");
+		return (0);
+	}
+	if (guess != SECRET)
+	{
+		puts("Wrong!");
+		return (1);
+	}
+	return (0);
+}
