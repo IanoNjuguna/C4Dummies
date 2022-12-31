feea318402cf6b0cbcd7b664b106d0119453f1e6
IanoNjuguna <inc3099@gmail.com>
2023-01-01 01:13:25 +0300

    ex0808-9 && Build

---
+++ BegC4D/Part2/Chapter8/ex0808-9/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
+{
+	int a, b;
+
+	a = 5;
+	b = -3;
+
+	if (a == b)
+	{
+		printf("%d equals %d\n", a, b);
+	}
+	else
+	{
+		printf("%d not equals %d\n", a, b);
+	}
+	return (0);
+}
+
