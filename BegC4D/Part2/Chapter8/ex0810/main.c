29f3d3dd51fe7778225931b70d0ad3bfa5341312
IanoNjuguna <inc3099@gmail.com>
2023-01-01 03:06:10 +0300

    ex0810 && Build

---
+++ BegC4D/Part2/Chapter8/ex0810/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjugua
+ */
+
+int main(void)
+{
+	int a, b;
+
+	a = 6;
+	b = a - 2;
+
+	if (a > b)
+	{
+		printf("%d is greater than %d\n", a, b);
+	}
+	else
+	{
+		printf("%d is greater than %d\n", a, b);
+	}
+	return (0);
+}
+
