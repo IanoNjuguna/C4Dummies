2a9ff09d718020e1a805e770621bbefcf4cf3822
IanoNjuguna <inc3099@gmail.com>
2023-01-01 04:09:24 +0300

    ex0815 && Build

---
+++ BegC4D/Part2/Chapter8/ex0815/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
+{
+	int coordinate;
+
+	printf("Input target coordinate: ");
+	scanf("%d", &coordinate);
+
+	if (coordinate <= -5 || coordinate >= 5)
+	{
+		puts("Close enough!");
+	}
+	else
+	{
+		puts("Target is out of range!");
+	}
+	return (0);
+}
+
