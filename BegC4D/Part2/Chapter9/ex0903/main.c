675bfc1393b018694e20ada8eb94350403a127c5
IanoNjuguna <inc3099@gmail.com>
2023-01-06 19:54:49 +0300

    ex0903 && Build

---
+++ BegC4D/Part2/Chapter9/ex0903/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = -5; count < 6; count++)
+	{
+		printf("%d\n", count);
+	}
+	return (0);
+}
+
