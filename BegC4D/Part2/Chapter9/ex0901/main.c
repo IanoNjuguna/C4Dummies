de6b09ef4169d33779016e9e1abf68102d88a9a1
IanoNjuguna <inc3099@gmail.com>
2023-01-06 18:49:35 +0300

    ex0901 && Build

---
+++ BegC4D/Part2/Chapter9/ex0901/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		puts("Sore shoulder sugery");
+	}
+	return (0);
+}
+
