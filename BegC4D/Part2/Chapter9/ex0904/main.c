cd3c1cfea90d749cc76e8970aeff5d4af54d41e3
IanoNjuguna <inc3099@gmail.com>
2023-01-06 20:06:09 +0300

    Order work

---
+++ BegC4D/Part2/Chapter9/ex0904/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 11; count <= 19; count++)
+	{
+		printf("%d\t", count);
+	}
+	printf("\n");
+
+	return (0);
+}
+
