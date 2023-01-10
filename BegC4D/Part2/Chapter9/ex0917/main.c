05db2a8a726669a37a44f1db2a65cc6a6818d6a4
IanoNjuguna <inc3099@gmail.com>
2023-01-10 16:13:45 +0300

    ex0917 && Build

---
+++ BegC4D/Part2/Chapter9/ex0917/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 13;
+
+	do {
+		puts("Sore shoulder surgery");
+	} while (x < 10);
+	return (0);
+}
+
