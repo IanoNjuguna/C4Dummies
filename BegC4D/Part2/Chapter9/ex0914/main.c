bf25e7dbeea85b255ac34eabd402e5b1c36e162f
IanoNjuguna <inc3099@gmail.com>
2023-01-10 15:33:28 +0300

    ex0914 && Build

---
+++ BegC4D/Part2/Chapter9/ex0914/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 13;
+
+	while (x < 10)
+	{
+		puts("Sore shoulder surgery");
+		x++;
+	}
+	return (0);
+}
+
