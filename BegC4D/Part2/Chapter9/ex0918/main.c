278155460edb89e3025d84fb489607292d38487b
IanoNjuguna <inc3099@gmail.com>
2023-01-12 12:29:52 +0300

    ex0918 && Build

---
+++ BegC4D/Part2/Chapter9/ex0918/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always Fail (Compile error)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x = 10; x++)
+	{
+		puts("What are you looking at?");
+	}
+	return (0);
+}
+
