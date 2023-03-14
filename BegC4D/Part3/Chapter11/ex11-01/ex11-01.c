2a323ff4e25640181373e2d1e6551c57e69c727b
IanoNjuguna <inc3099@gmail.com>
2023-03-14 10:06:59 +0300

    ex11-01 && Build

---
+++ BegC4D/Part3/Chapter11/ex11-01/ex11-01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Uses a for loop for iteration
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		puts("Get off my lawn, you kids.");
+	}
+	return (0);
+}
+
