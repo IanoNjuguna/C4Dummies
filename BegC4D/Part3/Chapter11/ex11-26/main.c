97d8e8ac2a53106d662eeba91d2d509beda0c025
IanoNjuguna <inc3099@gmail.com>
2023-04-14 17:39:52 +0300

    Addition and Subtraction take place before multiplication and division

---
+++ BegC4D/Part3/Chapter11/ex11-26/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Order of precedence
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a = ((20 - 5) * (2 + 42)) / 6;
+
+	printf("%d\n", a);
+
+	return (0);
+}
+
