9e38e6202f185608fe5657edf052b62968e74022
IanoNjuguna <inc3099@gmail.com>
2023-04-14 17:46:29 +0300

    Subtraction occurs before multiplication

---
+++ BegC4D/Part3/Chapter11/ex11-24/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Order of precedence
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int main(void)
+{
+	int a = (12 - 5) * 2;
+
+	printf("%d\n", a);
+	return (0);
+}
+
