05db982721bbca8ecafac31cc135af21af56c086
IanoNjuguna <inc3099@gmail.com>
2023-04-14 17:50:58 +0300

    Sequential Division

---
+++ BegC4D/Part3/Chapter11/ex11-23/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Order of precedence
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+int main(void)
+{
+	int a = (12 / 3) / 2;
+
+	printf("%d\n", a);
+	return (0);
+}
+
